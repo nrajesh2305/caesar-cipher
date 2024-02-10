f83a0982a151c70dac19b5415e5ed7988ce50057
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2024-02-09 17:36:10 -0800

    Finished decrypt_message() function. Works 100%.

--- hacker.py
+++ hacker.py
@@ -20,8 +20,16 @@ def encrypt_message(message, key):
         encrypted_message += letter.upper()
     return encrypted_message
 
-message = "Meet me by the rose bushes tonight."
-print(encrypt_message(message, 4))
-
+# Works 100%
 def decrypt_message(message, key):
-    pass
+    decrypted_message = ""
+    for letter in message:
+        if(letter.upper() not in alphabet):
+            decrypted_message += letter.upper()
+            continue
+        if(alphabet.index(letter.upper()) - key < 0):
+            letter = alphabet[len(alphabet) - key]
+        else:
+            letter = alphabet[alphabet.index(letter.upper()) - key]
+        decrypted_message += letter.upper()
+    return decrypted_message
