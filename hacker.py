c7a1a206099f4316d2540d5dff341754c83853f4
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2024-02-09 17:43:59 -0800

    Finished working on hacker.py

--- hacker.py
+++ hacker.py
@@ -10,26 +10,28 @@ alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'
 def encrypt_message(message, key):
     encrypted_message = ""
     for letter in message:
-        if(letter.upper() not in alphabet):
-            encrypted_message += letter.upper()
+        letter = letter.upper()
+        if(letter not in alphabet):
+            encrypted_message += letter
             continue
-        if(alphabet.index(letter.upper()) + key > len(alphabet)):
-            letter = alphabet[(alphabet.index(letter.upper()) + key) - len(alphabet)]
+        if(alphabet.index(letter) + key > len(alphabet)):
+            letter = alphabet[(alphabet.index(letter) + key) - len(alphabet)]
         else:
-            letter = alphabet[alphabet.index(letter.upper()) + key]
-        encrypted_message += letter.upper()
+            letter = alphabet[alphabet.index(letter) + key]
+        encrypted_message += letter
     return encrypted_message
 
 # Works 100%
 def decrypt_message(message, key):
     decrypted_message = ""
     for letter in message:
-        if(letter.upper() not in alphabet):
-            decrypted_message += letter.upper()
+        letter = letter.upper()
+        if(letter not in alphabet):
+            decrypted_message += letter
             continue
-        if(alphabet.index(letter.upper()) - key < 0):
-            letter = alphabet[len(alphabet) - key]
+        if(alphabet.index(letter) - key < 0):
+            letter = alphabet[len(alphabet) - alphabet.index(letter)]
         else:
-            letter = alphabet[alphabet.index(letter.upper()) - key]
-        decrypted_message += letter.upper()
+            letter = alphabet[alphabet.index(letter) - key]
+        decrypted_message += letter
     return decrypted_message
